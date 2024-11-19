be43e95cb863e9527814a81489c7673386618f6a
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-19 14:34:56 -0500

    Add files via upload

---
+++ date/getMonthDayYear.js
@@ -0,0 +1,23 @@
+// getMonthDayYear.js
+
+function getMonthDayYear()
+{
+    let dateTime = new Date();
+
+    let monthDayYear = dateTime.toLocaleDateString();
+
+    return monthDayYear;
+}
+
+// console.log(getMonthDayYear());
+
+/*
+11/18/2024
+*/
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
